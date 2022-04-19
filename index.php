d49bbe702038f15ab66c4aa6c983f3370f92a081
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-04-18 21:29:22 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+    <title>PHP com MySql</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</title>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-joao" id="navbar-top">
+  <a class="navbar-brand" id="navbar-logo" href="#">Logo</a>
+  <div class="m-nav" id="m-nav" >MEIO DA NAVBAR</div>
+ 
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <form class="form-inline my-2 my-lg-0" id="navbar-search">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+    </form>
+  </div>
+</nav>
+
+<div class="top-buttons">
+<button class="btn btn-outline-success my-2 my-sm-0" id="buttontop" type="submit">Button 1</button>
+<button class="btn btn-outline-success my-2 my-sm-0" id="buttontop" type="submit">Button 2</button>
+<button class="btn btn-outline-success my-2 my-sm-0" id="buttontop" type="submit">Button 3</button>
+<button class="btn btn-outline-success my-2 my-sm-0" id="buttontop" type="submit">Button 4</button>
+</div>
+
+<div class="div-menu">
+
+</div>
+
+</body>
+
+</html>

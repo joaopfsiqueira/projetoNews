5512190a7f253a350d48ba627c606b5fbf90fb99
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-04-21 19:27:22 -0300

    feat: criando tela de login

--- login.php
+++ login.php
@@ -23,55 +23,62 @@
 
 
 <div class="top-buttons">
-<a class="btn btn-primary" href="#" role="button" id="buttontop" >Home</a>
+<a class="btn btn-primary" href="index.php" role="button" id="buttontop" >Home</a>
 <a class="btn btn-primary" href="#" role="button" id="buttontop" >Fale Conosco</a>
 <a class="btn btn-primary" href="aboutus.php" role="button" id="buttontop" >Sobre Nós</a>
 <a class="btn btn-primary" href="login.php" role="button" id="buttontop" >Login</a>
 <a class="btn btn-primary" href="area_admin.php" role="button" id="buttontop" >Area Administrativa</a>
 </div>
 
-<div class="div-menu">
-  <div class="container">
-   <div class="row">
-      <div class="col">
-      1 of 3
-      </div>
-      <div class="col-6">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-           <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="d-block w-100" src="./img/foto1.jpg" alt="First slide">
+
+<div class="content" >
+    <div class="content-inside"> 
+    <section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Please enter your login and password!</p>
+
+              <div class="form-outline form-white mb-4">
+                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+                <label class="form-label" for="typeEmailX">Email</label>
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                <label class="form-label" for="typePasswordX">Password</label>
+              </div>
+
+              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+              <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+              </div>
+
             </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="./img/foto2.jpg" alt="Second slide">
+
+            <div>
+              <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+              </p>
             </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="./img/foto3.jpg" alt="Third slide">
-            </div>
+
           </div>
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
         </div>
-        <div class="div-text">
-          <div class="span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-        </div>
-      </div>
-      <div class="col">
-      3 of 3
       </div>
     </div>
   </div>
+</section>
+    </div>
 </div>
 
 
